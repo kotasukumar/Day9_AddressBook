51d14aa4a1cff896283edc641ffbde4fc902b7d2
sukumarkindia424@gmail.com <sukumarkindia424@gmail.com>
2022-03-23 20:10:01 +0530

    added:New contact

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -6,5 +6,22 @@ public class AddressBookMain {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
+
+        Add add = new Add();
+        add.firstName = "k";
+        add.lastName = "sukumar";
+        add.address = "2,abc colony";
+        add.city = "kfj";
+        add.state = "aa";
+        add.pinCode = 123456;
+        add.contactNumber = 987654321;
+        add.emailID = "abc@gmail.com";
+
+        ContactStorage contactStorage = new ContactStorage();
+        contactStorage.add1(add);
+
+        UserInterface userInterface = new UserInterface();
+        ArrayList contactList = contactStorage.getContactList();
+        userInterface.print(contactList);
     }
 }
